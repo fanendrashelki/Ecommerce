08f0ff27fed4c21815b6e9b6f22889bc1d6344cd
fanendrashelki <fanendrashelki@gmail.com>
2025-07-09 12:58:36 +0530

    cat slider

--- frontend/src/components/CatSlider/HomeCatSlider.jsx
+++ frontend/src/components/CatSlider/HomeCatSlider.jsx
@@ -1,12 +1,7 @@
 import { Link } from "react-router-dom";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
-// import required modules
 import { Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
 import axiosInstance from "../../utils/axiosInstance";
@@ -28,8 +23,8 @@ const HomeCatSlider = () => {
   }, []);
 
   return (
-    <div className="homecatslider pt-4 py-8 bg-gray-100">
-      <div className="container mx-auto">
+    <div className="homecatslider py-6 bg-gray-100">
+      <div className="px-3 sm:px-6 md:px-12 lg:px-20">
         <Swiper
           loop={true}
           navigation={true}
@@ -38,42 +33,24 @@ const HomeCatSlider = () => {
           modules={[Navigation]}
           className="mySwiper"
           breakpoints={{
-            320: {
-              slidesPerView: 2,
-              spaceBetween: 10,
-            },
-            480: {
-              slidesPerView: 3,
-              spaceBetween: 10,
-            },
-            640: {
-              slidesPerView: 4,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 5,
-              spaceBetween: 10,
-            },
-            1024: {
-              slidesPerView: 6,
-              spaceBetween: 10,
-            },
-            1280: {
-              slidesPerView: 8,
-              spaceBetween: 10,
-            },
+            320: { slidesPerView: 2.5, spaceBetween: 10 },
+            480: { slidesPerView: 3.5, spaceBetween: 12 },
+            640: { slidesPerView: 4.5, spaceBetween: 14 },
+            768: { slidesPerView: 5.5, spaceBetween: 16 },
+            1024: { slidesPerView: 6.5, spaceBetween: 18 },
+            1280: { slidesPerView: 8, spaceBetween: 20 },
           }}
         >
           {catList.map((cat, index) => (
             <SwiperSlide key={cat._id || index}>
               <Link to="/" className="block">
-                <div className="item py-5 px-2 sm:py-6 sm:px-3 bg-white rounded-lg text-center flex flex-col items-center justify-center shadow-sm hover:shadow-lg transition-shadow duration-200 transform hover:scale-105">
+                <div className="py-4 px-2 sm:py-5 sm:px-4 bg-white rounded-lg text-center flex flex-col items-center justify-center shadow-sm hover:shadow-md transition-transform duration-200 transform hover:scale-105">
                   <img
                     src={cat.image}
                     alt={cat.name}
-                    className="w-[60px] h-[60px] object-cover rounded-full border border-gray-200"
+                    className="w-14 h-14 sm:w-16 sm:h-16 object-cover rounded-full border border-gray-300"
                   />
-                  <h3 className="font-medium text-[14px] sm:text-[16px] mt-2 sm:mt-3 capitalize text-gray-800">
+                  <h3 className="font-medium text-[13px] sm:text-[15px] mt-2 sm:mt-3 capitalize text-gray-800 text-center truncate w-full">
                     {cat.name}
                   </h3>
                 </div>
