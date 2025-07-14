c88bb42a8de7d9114b13f41ace64c86d8e200d93
fanendrashelki <fanendrashelki@gmail.com>
2025-07-14 23:18:47 +0530

    filter

--- frontend/src/Pages/ProductListing.jsx
+++ frontend/src/Pages/ProductListing.jsx
@@ -4,15 +4,24 @@ import Link from "@mui/material/Link";
 import ProductItem from "../components/ProductItem/ProductItem";
 import ProductItemList from "../components/ProductItem/ProductItemList";
 import { Button } from "@mui/material";
-import { IoGridSharp } from "react-icons/io5";
+import { IoGridSharp, IoOptionsOutline } from "react-icons/io5";
 import { FiMenu } from "react-icons/fi";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import { useEffect, useState } from "react";
+import { useEffect, useState, forwardRef } from "react";
 import axiosInstance from "../utils/axiosInstance";
 import { useLocation, useParams } from "react-router-dom";
 import ProductItemSkeleton from "../components/Skeleton/ProductItemSkeleton";
 import ProductNotFound from "../components/ProductNotFound";
+import Dialog from "@mui/material/Dialog";
+import Slide from "@mui/material/Slide";
+import { MdClose } from "react-icons/md";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const ProductListing = () => {
   const { id } = useParams();
@@ -20,7 +29,10 @@ const ProductListing = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [productByCat, setProductByCat] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [filterOpen, setFilterOpen] = useState(false);
   const location = useLocation();
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("lg"));
 
   const queryParams = new URLSearchParams(location.search);
   const type = queryParams.get("type");
@@ -67,7 +79,7 @@ const ProductListing = () => {
   };
 
   return (
-    <section className="py-5 pb-0 bg-[#efefef] min-h-screen">
+    <section className="py-5 pb-24 bg-[#efefef] min-h-screen">
       <div className="container mx-auto px-3 sm:px-4">
         {/* Breadcrumbs */}
         <Breadcrumbs aria-label="breadcrumb">
@@ -92,8 +104,8 @@ const ProductListing = () => {
 
       <div className="bg-white p-2 mt-4">
         <div className="container mx-auto px-3 sm:px-4 flex flex-col lg:flex-row gap-3">
-          {/* Sidebar */}
-          <div className="w-full lg:w-[25%]">
+          {/* Desktop Sidebar */}
+          <div className="hidden lg:block w-full lg:w-[25%]">
             <Sidebar />
           </div>
 
@@ -196,6 +208,36 @@ const ProductListing = () => {
           </div>
         </div>
       </div>
+      <div>
+        <button
+          onClick={() => setFilterOpen(true)}
+          className="lg:hidden fixed bottom-[80px] right-4 z-50 bg-[#35ac75] text-white px-4 py-2 rounded-full text-sm shadow-lg flex items-center gap-2"
+        >
+          <IoOptionsOutline size={18} />
+          Filter
+        </button>
+      </div>
+
+      {/* Mobile Filter Drawer */}
+      <Dialog
+        fullScreen={fullScreen}
+        open={filterOpen}
+        onClose={() => setFilterOpen(false)}
+        TransitionComponent={Transition}
+      >
+        <div className="flex items-center justify-between p-4 border-b">
+          <h2 className="text-lg font-semibold">Filters</h2>
+          <Button
+            onClick={() => setFilterOpen(false)}
+            className="!min-w-[auto] !p-1 !text-black"
+          >
+            <MdClose size={24} />
+          </Button>
+        </div>
+        <div className="p-4">
+          <Sidebar />
+        </div>
+      </Dialog>
     </section>
   );
 };
