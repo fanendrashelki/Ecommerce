8606860e990cfc529362deb679a1691c1c539253
fanendrashelki <fanendrashelki@gmail.com>
2025-07-15 14:24:29 +0530

    login

--- Backend/utils/getToken.js
+++ Backend/utils/getToken.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
 
 const generateToken = (user) => {
   return jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
