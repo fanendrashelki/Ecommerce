6f1ed65e4f8e72f6b9909e6d13bbd0c1d12f5d50
fanendrashelki <fanendrashelki@gmail.com>
2025-07-17 17:54:32 +0530

    Add to cart list

--- frontend/src/components/ProductItem/ProductItem.jsx
+++ frontend/src/components/ProductItem/ProductItem.jsx
@@ -218,7 +218,7 @@ const ProductItem = ({ product }) => {
               to={`/product-details/${product._id}`}
               className="hover:text-[#35ac75]"
             >
-              {product._id}
+              {product.name}
             </Link>
           </h3>
 
